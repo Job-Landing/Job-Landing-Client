0642fb821515309fe63b3d410a273df33573b6ed
Maple <maple.zhu7@gmail.com>
2022-06-12 22:30:53 -0700

    add handle change to name and email

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,47 +4,59 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import {Link} from "react-router-dom";
 import Wrapper from "../asserts/wrappers/SignUpAndLogIn";
+const initialState = {
+  name: '',
+  email: '',
+  password: ''
+}
 
 const Signup = (() => {
-
-  const [password, setPassword] = useState('');
+  const [values, setValues] = useState(initialState);
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleChangePassword = (e) => {
-    setPassword(e.target.value)
+  const handleChange = (e) => {
+    setValues({...values, [e.target.name]: e.target.value})
   }
 
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword)
   };
 
-  const handleMouseDownPassword = (e) => {
+  // const handleMouseDownPassword = (e) => {
+  //   e.preventDefault();
+  // };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
+    const {name, email, password} = values
   };
 
   return (
-    <Wrapper>
-      {/* <h1>Signup</h1> */}
-      <img className="logo" src={logo} alt="logo" />
-      <div className="form_wrapper">
-        <div className="form_wrapper_1">
-          <h3 className="form_title">Sign Up</h3>
-        </div>
-        <form>
-          <input autoComplete="off" type="email" name="email" placeholder='E-mail'/>
-          <input autoComplete="off" type="text" name="name" placeholder='Username'/>
-          <input autoComplete="off" type={!showPassword ? "password" : "text"} name="password" placeholder='Password'
-                 onChange={handleChangePassword}/>
-          {!showPassword ? <VisibilityOffIcon onClick={handleClickShowPassword} className='seen'/> :
-              <VisibilityIcon onClick={handleClickShowPassword} className='seen'/>}
+      <Wrapper>
+        {/* <h1>Signup</h1> */}
+        <img className="logo" src={logo} alt="logo"/>
+        <div className="form_wrapper">
+          <div className="form_wrapper_1">
+            <h3 className="form_title">Sign Up</h3>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <input autoComplete="off" type="email" name="email" value={values.email} placeholder='E-mail'
+                   onChange={handleChange}/>
+            <input autoComplete="off" type="text" name="name" value={values.name} placeholder='Username'
+                   onChange={handleChange}/>
+            <input autoComplete="off" type={!showPassword ? "password" : "text"} name="password" value={values.password}
+                   placeholder='Password'
+                   onChange={handleChange}/>
+            {!showPassword ? <VisibilityOffIcon onClick={handleClickShowPassword} className='seen'/> :
+                <VisibilityIcon onClick={handleClickShowPassword} className='seen'/>}
 
-          {/*<p>Forgot your password?</p>*/}
-          <button className='submit'>Sign Up</button>
-        </form>
-        <div className='redirect'>
-          <Link className='link' to="/login"><p>Already has an account? Login</p></Link>
+            {/*<p>Forgot your password?</p>*/}
+            <button className='submit'>Sign Up</button>
+          </form>
+          <div className='redirect'>
+            <Link className='link' to="/login"><p>Already has an account? Login</p></Link>
+          </div>
         </div>
-      </div>
     </Wrapper>
 
   )

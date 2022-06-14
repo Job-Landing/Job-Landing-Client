49a1b2b5b7d8ad5a3f38bfce9f8fe14234c97584
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2022-06-14 10:38:11 -0700

    add: get add job form value

--- src/components/AddJobForm.js
+++ src/components/AddJobForm.js
@@ -8,17 +8,50 @@ import Select from '@mui/material/Select';
 import DatePicker from 'react-date-picker';
 import Button from '@mui/material/Button';
 
+// async function submitJobForm(email, name) {
+//     return fetch('URL', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             email: email,
+//             name: name
+//         })
+//     })
+//     .then(data => data.json())
+// }
+
 const AddJobForm = () => {
+    const [position, setPosition] = useState('');
+    const [company, setCompany] = useState('');
+    const [location, setLocation] = useState('');
 
-    const [status, setStatus] = React.useState('');
-    const [type, setType] = React.useState('');
-    const [value, onChange] = useState(new Date());
+    const [status, setStatus] = useState('');
+    const [type, setType] = useState('');
+    const [date, setDate] = useState(new Date());
 
-    const handleChange = (event) => {
-        setStatus(event.target.value);
+    const submitJob = (e) => {
+
+    }
+    const clearJob = (e) => {
+
+    }
+    // get form value
+    const onChangePassword = (e) => {
+        setPosition(e.target.value)
+    }
+    const onChangeCompany = (e) => {
+        setCompany(e.target.value)
+    }
+    const onChangeLocation = (e) => {
+        setLocation(e.target.value)
+    }
+    const handleChangeStatus = (e) => {
+        setStatus(e.target.value);
     };
-    const handleTypeChange = (event) => {
-        setType(event.target.value);
+    const handleTypeChange = (e) => {
+        setType(e.target.value);
     };
 
     return (
@@ -28,15 +61,15 @@ const AddJobForm = () => {
                     <div className="form_row1">
                         <div>
                             <label htmlFor="position" className="label">Position</label> <br />
-                            <input className="input" type="text" name="position" />
+                            <input className="input" type="text" name="position" onChange={onChangePassword} value={position} />
                         </div>
                         <div>
-                            <label htmlFor="position" className="label">Company</label> <br />
-                            <input className="input" type="text" name="Company" />
+                            <label htmlFor="company" className="label">Company</label> <br />
+                            <input className="input" type="text" name="Company" onChange={onChangeCompany} value={company} />
                         </div>
                         <div>
-                            <label htmlFor="position" className="label">Location</label> <br />
-                            <input className="input" type="text" name="location" />
+                            <label htmlFor="location" className="label">Location</label> <br />
+                            <input className="input" type="text" name="location" onChange={onChangeLocation} value={location} />
                         </div>
                     </div>
 
@@ -50,7 +83,7 @@ const AddJobForm = () => {
                                         id="demo-simple-select1"
                                         value={status}
                                         label="Status"
-                                        onChange={handleChange}
+                                        onChange={handleChangeStatus}
                                     >
                                         <MenuItem value='pending'>Pending</MenuItem>
                                         <MenuItem value='interview'>Interview</MenuItem>
@@ -80,12 +113,12 @@ const AddJobForm = () => {
                             </Box>
                         </div>
                         <div>
-                            <DatePicker onChange={onChange} value={value} className='date_picker' />
+                            <DatePicker onChange={setDate} value={date} className='date_picker' />
                         </div>
                     </div>
                     <div className='btn'>
-                        <Button variant="contained" className='submit'>Submit</Button>
-                        <Button variant="contained" className='clear'>Clear</Button>
+                        <Button variant="contained" className='submit' onClick={submitJob}>Submit</Button>
+                        <Button variant="contained" className='clear' onClick={clearJob}>Clear</Button>
                     </div>
                 </form>
             </div>

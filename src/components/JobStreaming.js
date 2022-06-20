9265c9cabb5ffe28a792b154d45a463b37fe81d8
Maple <maple.zhu7@gmail.com>
2022-06-20 13:07:24 -0700

    Add Job Streaming page

---
+++ src/components/JobStreaming.js
@@ -0,0 +1,88 @@
+import React, {useState, useEffect} from 'react'
+import Wrapper from '../asserts/wrappers/JobStreaming'
+import axios from 'axios';
+import {BASE_URL} from '../utils/constant'
+
+const JobStreaming = () => {
+    const path = window.location.pathname.replace("/", "")
+
+    const [streamItem, setStreamItem] = useState([]);
+
+    const getJobStreamingList = async () => {
+        const response = await axios(`${BASE_URL}/stream`)
+        if (response.status === 200) {
+            setStreamItem(response.data)
+            console.log(response.data)
+            // console.log(streamItem)
+        }
+    }
+
+    useEffect(() => {
+        getJobStreamingList()
+    }, [])
+
+    return (
+        <Wrapper className="animate__animated animate__fadeIn">
+            <div className='inner_table'>
+                <h2>Job Streaming</h2>
+                <table className='information_table'>
+                    <tbody>
+                    <tr className='tr'>
+                        <th>Position</th>
+                        <th>Company</th>
+                        <th>Apply Url</th>
+                        <th>Create Time</th>
+                        <th className='last_column'>Status</th>
+                    </tr>
+                    {path === 'jobstream' ?
+                        streamItem.map((item) => {
+                            return <tr key={item._id}>
+                                <td>{item.position}</td>
+                                <td>{item.company}</td>
+                                <td>{item.applyUrl}</td>
+                                <td>May 23, 2022</td>
+                                <td><span className='pending'>pending</span></td>
+                            </tr>
+                        })
+                        :
+                        streamItem.slice(0, 5).map((item) => {
+                            return <tr key={item._id}>
+                                <td>{item.position}</td>
+                                <td>{item.company}</td>
+                                <td>{item.applyUrl}</td>
+                                <td>May 23, 2022</td>
+                                <td><span className='pending'>pending</span></td>
+                            </tr>
+                        })
+                    }
+
+                    <tr>
+                        <td>Software Engineer Intern</td>
+                        <td>Google</td>
+                        <td>https://www.google.com/</td>
+                        <td>May 23, 2022</td>
+                        <td><span className='interview'>interview</span></td>
+                    </tr>
+                    <tr>
+                        <td>Software Engineer Intern</td>
+                        <td>Google</td>
+                        <td>https://www.google.com/</td>
+                        <td>May 23, 2022</td>
+                        <td><span className='offer'>offer</span></td>
+                    </tr>
+                    <tr>
+                        <td>Software Engineer Intern</td>
+                        <td>Google</td>
+                        <td>https://www.google.com/</td>
+                        <td>May 23, 2022</td>
+                        <td><span className='decline'>decline</span></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </Wrapper>
+    );
+}
+
+
+export default JobStreaming

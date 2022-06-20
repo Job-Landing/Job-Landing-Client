ba3269bb687dd6f3f229c402898c3ec3ca27eb1a
Maple <maple.zhu7@gmail.com>
2022-06-20 12:57:57 -0700

    Extract Wrappers from components

--- src/components/ProfileUpdate.js
+++ src/components/ProfileUpdate.js
@@ -1,12 +1,8 @@
-import React,{useState} from 'react'
-import styled from 'styled-components'
-import FaceRoundedIcon from '@mui/icons-material/FaceRounded';
-import avatar from '../images/avatar.jpg'
+import React, {useState} from 'react'
+import avatar from '../asserts/images/avatar.jpg'
 import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-
-
+import Wrapper from '../asserts/wrappers/ProfileUpdate'
 
 const ProfileUpdate = () => {
     const [updateStatus, setUpdateStatus] = useState('none');
@@ -19,18 +15,6 @@ const ProfileUpdate = () => {
     return (
         <Wrapper className='animate__animated animate__fadeIn'>
             <div>
-                <div className='box_wrapper_bottom'>
-                    <div className='box_wrapper_top'>
-                        <div className='box_wrapper1'>
-                            <FaceRoundedIcon className='icon' />
-                        </div>
-                        <div className='box_wrapper2'>
-                            <h5>Profile</h5>
-                            <div className='row1'></div>
-                            <div className='row2'></div>
-                        </div>
-                    </div>
-                </div>
                 <div className='information_wrapper'>
                     <img className='avatar' src={avatar} alt="avatar" />
                     <div className='information'>
@@ -64,120 +48,6 @@ const ProfileUpdate = () => {
     );
 }
 
-const Wrapper = styled.section`
-    padding-top: 20px;
 
-    .box_wrapper_bottom{
-        /* background-color: #ABA5EA; */
-        background-color: rgba(171,165,234, 0.4);
-        width: 280px;
-        border-radius: 15px;
-    }
-    .box_wrapper_top{
-        display: flex;
-        background-color: #8489ba;
-        width: 270px;
-        border-radius: 15px;
-        padding: 10px;
-        transform: rotate(4deg);
-    }
-    .box_wrapper1{
-        width: 50px;
-        height: 50px;
-        background-color: white;
-        border-radius: 10px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    .box_wrapper2{
-        padding-left: 10px;
-        display: flex;
-        flex-direction: column;
-        justify-content:space-between;
-
-    }
-    .icon{
-        color: #17171e;
-        width: 35px;
-        height: 35px;
-    }
-    h5{
-        margin: 0;
-    }
-    .row1{
-        width: 200px;
-        height: 7px;
-        background-color: white;
-        border-radius: 10px;
-    }
-    .row2{
-        width: 150px;
-        height: 7px;
-        background-color: white;
-        border-radius: 10px;
-    }
-    .information_wrapper{
-        display: flex;
-        align-items: center;
-        padding: 40px 10px 10px 0;
-    }
-    .avatar{
-        width: 150px;
-        height: 150px;
-        border-radius:50%;
-    }
-    .information{
-        margin-left: 20px;
-    }
-    .information p{
-        font-size: 20px;
-        font-weight: 500;
-    }
-
-    .form_row{
-        display: block;
-        margin-bottom: 20px;
-    }
-    input{
-        width:  400px;
-        height: 30px;
-        border-radius:2px;
-        transition: 0.3s;
-        border-style: solid;
-        border:0;
-        outline:0;
-        padding: 2px 0 2px 10px;
-        font-weight: 500;
-        margin-top: 8px;
-    }
-    input:disabled {
-        background-color: #696d94;
-        color: #1e203b;
-    }
-    .update{
-        margin-top: 10px;
-    }
-    .form_wrapper{
-        margin-top: 20px;
-    }
-    img{
-        width: 500px;
-    }
-
-    .alert{
-        width: 200px;
-        position: fixed;
-        top: 120px;
-        right: 70px;
-    }
-
-    .none{
-        display: none;
-    }
-
-
-
-`
 
 export default ProfileUpdate
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react'
-import styled from 'styled-components'
-import avatar from '../images/avatar.jpg'
-import { Link, useLocation } from 'react-router-dom'
+import avatar from '../asserts/images/avatar.jpg'
+import {Link, useLocation} from 'react-router-dom'
 import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
 import WorkRoundedIcon from '@mui/icons-material/WorkRounded';
-import WorkHistoryRoundedIcon from '@mui/icons-material/WorkHistoryRounded';
 import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import Wrapper from '../asserts/wrappers/Sidebar'
+import StreamIcon from '@mui/icons-material/Stream';
 
 
 const Sidebar = () => {
@@ -13,9 +13,9 @@ const Sidebar = () => {
   const [pathname, setPathname] = useState(path);
 
   return (
-    <Wrapper>
-      <div className='profile'>
-        <img src={avatar} alt="avatar" />
+      <Wrapper>
+        <div className='profile'>
+          <img src={avatar} alt="avatar"/>
         <div className='profile_info'>
           <span>Zanyuan Yang</span>
           <span>user</span>
@@ -37,83 +37,24 @@ const Sidebar = () => {
       <div>
         <h2 className='title'>Management</h2>
         <div className='general-item '>
-          <div className='item out-active'>
-            <DashboardRoundedIcon className='icon' />
+          <Link to='/joblist' className={path === 'joblist' ? 'item' : 'item out-active'}>
+            <DashboardRoundedIcon className='icon'/>
             <p>All Jobs</p>
-          </div>
-          <div className='item out-active'>
-            <WorkRoundedIcon className='icon' />
+          </Link>
+          <Link to='/addjob' className={path === 'addjob' ? 'item' : 'item out-active'}>
+            <WorkRoundedIcon className='icon'/>
             <p>Add Job</p>
-          </div>
+          </Link>
+          <Link to='/jobstreaming' className={path === 'jobstreaming' ? 'item' : 'item out-active'}>
+            <StreamIcon className='icon'/>
+            <p>Job Streaming</p>
+          </Link>
         </div>
       </div>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.section`
-  padding: 40px 20px 0px 20px;
-  width: 280px;
-  border-right: 1px dashed #696d94;
-  .profile{
-    display: flex;
-    background-color:  #8489ba;
 
-    width: 240px;
-    padding: 15px 15px 15px 20px;
-    border-radius: 20px;
-  }
-  .profile_info{
-
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    padding-left: 15px;
-    color: black;
-  }
-
-  span:nth-child(1){
-    font-weight: 500;
-    font-size: 16px;
-  }
-  span:nth-child(2){
-    font-weight: 400;
-    font-size: 14px;
-  }
-  .item{
-
-    display: flex;
-    background-color: #696d94;
-    width: 250px;
-    border-radius: 10px;
-    padding: 15px 15px 15px 20px;
-    margin-bottom: 10px;
-    color: #1e203b;
-    text-decoration: none;
-  }
-  p{
-
-    margin-top: 0;
-    margin-bottom: 0;
-    margin-left: 20px;
-    font-weight: 500;
-    font-size: 16px;
-  }
-  .out-active{
-    color: white !important;
-    background-color:transparent !important;
-  }
-  img{
-
-    width: 60px;
-    height: 60px;
-    border-radius: 50%;
-  }
-  .title{
-
-    margin-top: 40px;
-    margin-left: 20px;
-  }
-`
 
 export default Sidebar

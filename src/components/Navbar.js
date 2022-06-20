f2ec8fd8f20d0d70c39c47654f73f1eb676f0e20
Maple <maple.zhu7@gmail.com>
2022-06-20 12:28:17 -0700

    Add Side Bar&Nav Bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,134 @@
-import React from 'react'
-import avatar from '../assets/images/avatar.jpg'
-import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
-import WorkRoundedIcon from '@mui/icons-material/WorkRounded';
-import WorkHistoryRoundedIcon from '@mui/icons-material/WorkHistoryRounded';
-import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
-import Wrapper from '../assets/wrappers/NavBar';
+import React, {useState} from 'react';
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+import avatar from '../images/avatar.jpg'
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 
-const Navbar = () => {
-  return (
-    <Wrapper>
-      <div className='profile'>
-        <img src={avatar} alt="avatar" />
-        <div className='profile_info'>
-          <span>Zanyuan Yang</span>
-          <span>user</span>
-        </div>
-      </div>
-      <div>
-        <h2 className='title'>GENERAL</h2>
-        <div className='general-item'>
-          <div className='item'>
-            <DashboardRoundedIcon className='icon' />
-            <p>Dashboard</p>
-          </div>
-          <div className='item out-active'>
-            <PersonRoundedIcon className='icon' />
-            <p>Profile</p>
-          </div>
-        </div>
-      </div>
-      <div>
-        <h2 className='title'>Management</h2>
-        <div className='general-item '>
-          <div className='item out-active'>
-            <DashboardRoundedIcon className='icon' />
-            <p>All Jobs</p>
-          </div>
-          <div className='item out-active'>
-            <WorkRoundedIcon className='icon' />
-            <p>Add Job</p>
-          </div>
-        </div>
-      </div>
-    </Wrapper>
-  );
+const NavBar = () => {
+    const [selectOpen, setSelectOpen] = useState(false);
+
+    const openSelect = () => {
+        setSelectOpen(!selectOpen)
+    }
+
+    return (
+        <Wrapper>
+            <div className='navbar_wrapper'>
+                <SearchRoundedIcon className='icon'/>
+                <img src={avatar} alt="avatar" onClick={openSelect}/>
+            </div>
+            <div className='arrow_up' style={selectOpen ? {visibility: "visible"} : {visibility: "hidden"}}></div>
+            <div className='select' style={selectOpen ? {visibility: "visible"} : {visibility: "hidden"}}>
+                <p>Zanyuan Yang</p>
+                <p>jayingyoung@gmail.com</p>
+                <hr></hr>
+                <p>Home</p>
+                <p>Profile</p>
+                <hr/>
+                <p>Logout</p>
+            </div>
+
+        </Wrapper>
+    );
 }
 
-export default Navbar
+const Wrapper = styled.section`
+  padding-top: 20px;
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+
+  .navbar_wrapper {
+    display: flex;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+  }
+
+  .icon {
+    margin-right: 15px;
+    margin-top: 5px;
+  }
+
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-right: 30px;
+    transition: 0.5s;
+    cursor: pointer;
+  }
+
+  img:hover {
+    transform: scale(1.2);
+  }
+
+  .arrow_up {
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-left: 7px solid transparent;
+    border-right: 7px solid transparent;
+    border-bottom: 7px solid #696d94;
+    margin-top: 53px;
+    margin-right: 46px;
+
+  }
+
+  .select {
+    position: absolute;
+    height: 215px;
+    width: 200px;
+    margin-top: 60px;
+    margin-right: 30px;
+    /* background-color: #696d94; */
+    background-color: rgba(124, 129, 175, 0.9);
+    backdrop-filter: blur(6px);
+    border-radius: 15px;
+  }
+
+  p {
+    margin-top: 0;
+    margin-bottom: 0;
+    color: #1e203b;
+    padding-left: 20px;
+  }
+
+  p:nth-child(1) {
+    font-size: 15px;
+    font-weight: 500;
+    margin-bottom: 0;
+    margin-top: 10px;
+  }
+
+  p:nth-child(2) {
+    color: #2a2c52;
+    font-size: 12px;
+    font-weight: 500;
+    margin-top: 5px;
+    margin-bottom: 2px;
+  }
+
+  p:nth-child(4), p:nth-child(5), p:nth-child(7) {
+    font-size: 15px;
+    font-weight: 500;
+    margin-bottom: 0;
+    margin-top: 10px;
+    width: 165px;
+    padding: 5px 0 5px 15px;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 10px;
+  }
+
+  p:nth-child(4):hover, p:nth-child(5):hover, p:nth-child(7):hover {
+    background-color: #D6D5A8;
+    cursor: pointer;
+  }
+
+  hr {
+    border-bottom: 1px dashed white;
+  }
+`
+
+export default NavBar;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import styled from 'styled-components'
+import avatar from '../images/avatar.jpg'
+import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
+import WorkRoundedIcon from '@mui/icons-material/WorkRounded';
+import WorkHistoryRoundedIcon from '@mui/icons-material/WorkHistoryRounded';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+
+const Sidebar = () => {
+  return (
+      <Wrapper>
+        <div className='profile'>
+          <img src={avatar} alt="avatar"/>
+          <div className='profile_info'>
+            <span>Zanyuan Yang</span>
+            <span>user</span>
+          </div>
+        </div>
+        <div>
+          <h2 className='title'>GENERAL</h2>
+          <div className='general-item'>
+            <div className='item'>
+              <DashboardRoundedIcon className='icon'/>
+              <p>Dashboard</p>
+            </div>
+            <div className='item out-active'>
+              <PersonRoundedIcon className='icon'/>
+              <p>Profile</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className='title'>Management</h2>
+          <div className='general-item '>
+            <div className='item out-active'>
+              <DashboardRoundedIcon className='icon'/>
+              <p>All Jobs</p>
+            </div>
+            <div className='item out-active'>
+              <WorkRoundedIcon className='icon'/>
+              <p>Add Job</p>
+            </div>
+          </div>
+        </div>
+      </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  padding: 40px 20px 0px 20px;
+  width: 280px;
+  border-right: 1px dashed #696d94;
+
+  .profile {
+    display: flex;
+    background-color: #8489ba;
+    width: 240px;
+    padding: 15px 15px 15px 20px;
+    border-radius: 20px;
+  }
+
+  .profile_info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    padding-left: 15px;
+    color: black;
+  }
+
+  span:nth-child(1) {
+    font-weight: 500;
+    font-size: 16px;
+  }
+
+  span:nth-child(2) {
+    font-weight: 400;
+    font-size: 14px;
+  }
+
+  .item {
+    display: flex;
+    background-color: #696d94;
+    width: 250px;
+    border-radius: 10px;
+    padding: 15px 15px 15px 20px;
+    margin-bottom: 10px;
+    color: #1e203b;
+  }
+
+  p {
+    margin-top: 0;
+    margin-bottom: 0;
+    margin-left: 20px;
+    font-weight: 500;
+    font-size: 16px;
+  }
+
+  .out-active {
+    color: white !important;
+    background-color: transparent !important;
+  }
+
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+  }
+
+  .title {
+    margin-top: 40px;
+    margin-left: 20px;
+  }
+`
+
+export default Sidebar

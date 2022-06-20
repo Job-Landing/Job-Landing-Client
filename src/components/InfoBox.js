5b9777a88acbe8a7f5df23db2ee33f0deec08269
Maple <maple.zhu7@gmail.com>
2022-06-20 13:06:53 -0700

    Add Info box

---
+++ src/components/InfoBox.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+import FormatListBulletedRoundedIcon from '@mui/icons-material/FormatListBulletedRounded';
+import FaceRoundedIcon from '@mui/icons-material/FaceRounded';
+import AddTaskRoundedIcon from '@mui/icons-material/AddTaskRounded';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import Wrapper from '../asserts/wrappers/InfoBox'
+
+
+const InfoBox = () => {
+    const path = window.location.pathname.replace("/", "")
+
+
+    return (
+        // <Wrapper className="animate__animated animate__fadeIn"></Wrapper>
+        <Wrapper>
+            <div className='box_wrapper_bottom'>
+                <div className='box_wrapper_top'>
+                    <div className='box_wrapper1'>
+                        {
+                            path === 'profile' ? <FaceRoundedIcon className='icon'/> : path === 'joblist' ?
+                                <FormatListBulletedRoundedIcon className='icon'/> : path === 'addjob' ?
+                                    <AddTaskRoundedIcon className='icon'/> : <LiveTvIcon className='icon'/>
+                        }
+                    </div>
+                    <div className='box_wrapper2'>
+                        <h5>{path === 'profile' ? 'Profile' : path === 'joblist' ? 'Job List' : path === 'addjob' ? 'Add Job' : 'Job Streaming'}</h5>
+                        <div className='row1'></div>
+                        <div className='row2'></div>
+                    </div>
+                </div>
+            </div>
+        </Wrapper>
+    );
+}
+
+export default InfoBox

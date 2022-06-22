7979d2b92092b57f1faf599ed0174ade31b3ac8b
Maple <maple.zhu7@gmail.com>
2022-06-22 14:32:24 -0700

    Add anchor in dashboard to all job list

--- src/components/DashboardTable.js
+++ src/components/DashboardTable.js
@@ -1,9 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Button from '@mui/material/Button';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
 import Wrapper from '../asserts/wrappers/DashboardTable'
+import axios from 'axios';
+import {BASE_URL} from '../utils/constant'
+
 
 const DashboardTable = () => {
+    const [dashboardItem, setDashboardItem] = useState([]);
+
+    const getDashboardItemList = async () => {
+        const response = await axios(`${BASE_URL}/job`);
+        if (response.status === 200) {
+            setDashboardItem(response.data)
+            console.log(response.data)
+        }
+    }
+
+    useEffect(() => {
+        getDashboardItemList()
+    }, [])
+
     return (
         <Wrapper className='animate__animated animate__zoomInDown'>
             <div className='table_wrapper'>
@@ -12,6 +29,7 @@ const DashboardTable = () => {
                         <h2>Information</h2>
                         <table className='information_table'>
                             <tbody>
+
                                 <tr className='tr'>
                                     <th>Position</th>
                                     <th>Company</th>
@@ -19,6 +37,7 @@ const DashboardTable = () => {
                                     <th>Status</th>
                                     <th className='last_column'>Action</th>
                                 </tr>
+                                {/*TODO: link DB*/}
                                 <tr>
                                     <td>Software Engineer Intern</td>
                                     <td>Google</td>
@@ -26,64 +45,36 @@ const DashboardTable = () => {
                                     <td><span className='pending'>pending</span></td>
                                     <td>Detail</td>
                                 </tr>
-                                <tr>
-                                    <td>Software Engineer Intern</td>
-                                    <td>Google</td>
-                                    <td>May 23, 2022</td>
-                                    <td><span className='interview'>interview</span></td>
-                                    <td>Detail</td>
-                                </tr>
-                                <tr>
-                                    <td>Software Engineer Intern</td>
-                                    <td>Google</td>
-                                    <td>May 23, 2022</td>
-                                    <td><span className='offer'>offer</span></td>
-                                    <td>Detail</td>
-                                </tr>
-                                <tr>
-                                    <td>Software Engineer Intern</td>
-                                    <td>Google</td>
-                                    <td>May 23, 2022</td>
-                                    <td><span className='decline'>decline</span></td>
-                                    <td>Detail</td>
-                                </tr>
                             </tbody>
                         </table>
-                        <Button className='view_all' variant="text">View All <ChevronRightRoundedIcon/> </Button>
+                        <a href="/joblist"><Button className='view_all' variant="text">View
+                                                                                       All <ChevronRightRoundedIcon/>
+                        </Button></a>
+
                     </div>
 
                     <div className='inner_table'>
                         <h2>Interview</h2>
                         <table className='information_table'>
                             <tbody>
-                                <tr className='tr'>
-                                    <th>Position</th>
-                                    <th>Company</th>
-                                    <th className='last_column'>Date / Time</th>
-                                </tr>
-                                <tr>
-                                    <td>Software Engineer Intern</td>
-                                    <td>Google</td>
-                                    <td>May 23, 2022</td>
-                                </tr>
-                                <tr>
-                                    <td>Software Engineer Intern</td>
-                                    <td>Google</td>
-                                    <td>May 23, 2022</td>
-                                </tr>
-                                <tr>
-                                    <td>Software Engineer Intern</td>
-                                    <td>Google</td>
-                                    <td>May 23, 2022</td>
-                                </tr>
-                                <tr>
-                                    <td>Software Engineer Intern</td>
-                                    <td>Google</td>
-                                    <td>May 23, 2022</td>
-                                </tr>
+                            <tr className='tr'>
+                                <th>Position</th>
+                                <th>Company</th>
+                                <th className='last_column'>Date / Time</th>
+                            </tr>
+
+                            {/*TODO: link DB*/}
+                            <tr>
+                                <td>Software Engineer Intern</td>
+                                <td>Google</td>
+                                <td>May 23, 2022</td>
+                            </tr>
+
                             </tbody>
                         </table>
-                        <Button className='view_all' variant="text">View All <ChevronRightRoundedIcon/> </Button>
+                        <a href="/job/interview"><Button className='view_all' variant="text">View
+                                                                                             All <ChevronRightRoundedIcon/>
+                        </Button></a>
                     </div>
                 </div>
 

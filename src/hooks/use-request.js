bba7a74a3778ded2dd2e056645834589e618904e
wayson <wayson1225@outlook.com>
2022-06-25 18:19:01 -0700

    Add: display errors

---
+++ src/hooks/use-request.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const useRequest = ({ url, method, body, onSuccess }) => {
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async (props = {}) => {
+    try {
+      // clear the errors when new request
+      setErrors(null);
+      const response = await axios[method](url, {
+        ...body,
+        ...props,
+      });
+
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      setErrors(
+        <div>
+          <h4>Oooops...</h4>
+          <ul>
+            {error.response.data.errors.map((err) => (
+              <li key={err.field}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return { doRequest, errors };
+};
+
+export default useRequest;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,81 +1,124 @@
-import React, {useState} from 'react'
-import Wrapper from "../asserts/wrappers/SignUpAndLogIn";
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import { Link } from "react-router-dom";
-import { HomeNavbar } from '../components'
-import loginImg from '../asserts/images/login.png'
-import { BASE_URL } from '../utils/constant'
+import React, { useState } from 'react';
+import Wrapper from '../asserts/wrappers/SignUpAndLogIn';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import { Link } from 'react-router-dom';
+import { HomeNavbar } from '../components';
+import loginImg from '../asserts/images/login.png';
+import { BASE_URL } from '../utils/constant';
 import axios from 'axios';
-import {JobLandingContext} from '../context/context';
+import { JobLandingContext } from '../context/context';
+import useRequest from '../hooks/use-request';
 
-const Login = (() => {
-    const { login } = React.useContext(JobLandingContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
-    const [correctPassword, setCorrectPassword] = useState(true);
+const Login = () => {
+  const { login } = React.useContext(JobLandingContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [correctPassword, setCorrectPassword] = useState(true);
 
-    const handleChangeEmail = (e) => {
-        setEmail(e.target.value)
-    }
-    const handleChangePassword = (e) => {
-        setPassword(e.target.value)
-    }
+  const { doRequest, errors } = useRequest({
+    url: `${BASE_URL}/user/signin`,
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => {
+      //   Router.push('/');
+    },
+  });
 
-    const handleClickShowPassword = () => {
-        setShowPassword(!showPassword)
-    };
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const loginValidator = async (e) => {
-        e.preventDefault();
-        try {
-            const user = {email: email, password: password}
-            const response = await axios.post(`${BASE_URL}/user/signin`, user)
-            if (!response) {
-                throw new Error('wrong password or email')
-            }
-            login(response.data);
-            console.log(response);
-        } catch (err) {
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-            // console.log(err)
-        }
+  const loginValidator = async (e) => {
+    e.preventDefault();
+    await doRequest();
+    // can get errors from doRequest
+    console.log(errors);
 
-    }
+    // try {
+    //   const user = { email: email, password: password };
+    //   const response = await axios.post(`${BASE_URL}/user/signin`, user);
+    //   if (!response) {
+    //     throw new Error('wrong password or email');
+    //   }
+    //   login(response.data);
+    //   console.log(response);
+    // } catch (err) {
+    //   // console.log(err)
+    // }
+  };
 
-    return (
-        <Wrapper>
-            <HomeNavbar />
-            <main className='main'>
-                <div className='left'>
-                    <div className='left_inner'>
-                        <p className='text_login'>Hi, Welcome Back</p>
-                        <img className='register_img' src={loginImg} alt="register" />
-                    </div>
-                </div>
-                <div className='right'>
-                    <div className="form_wrapper">
-                        <p className='login'>Don’t have an account? <Link to='/signup' className='link link_to_login'>Get started</Link></p>
-                        <div className="form_wrapper_1">
-                            <h3 className="form_title">Sign in to Job Landing</h3>
-                            <p className='form_title1'>Enter your details below.</p>
-                        </div>
-                        <form onSubmit={loginValidator}>
-                            <input autoComplete="off" type="email" name="email" placeholder='E-mail' onChange={handleChangeEmail} required />
-                            <input autoComplete="off" type={!showPassword ? "password" : "text"} name="password" placeholder='Password'
-                                onChange={handleChangePassword} required />
-                            {!showPassword ? <VisibilityOffIcon onClick={handleClickShowPassword} className='seen'/> :
-                                <VisibilityIcon onClick={handleClickShowPassword} className='seen' />}
-                            <button className='submit' type='submit'>Log In</button>
-                        </form>
-                    </div>
-                </div>
-            </main>
-        </Wrapper>
+  return (
+    <Wrapper>
+      <HomeNavbar />
+      <main className="main">
+        <div className="left">
+          <div className="left_inner">
+            <p className="text_login">Hi, Welcome Back</p>
+            <img className="register_img" src={loginImg} alt="register" />
+          </div>
+        </div>
+        <div className="right">
+          <div className="form_wrapper">
+            <p className="login">
+              Don’t have an account?{' '}
+              <Link to="/signup" className="link link_to_login">
+                Get started
+              </Link>
+            </p>
+            <div className="form_wrapper_1">
+              <h3 className="form_title">Sign in to Job Landing</h3>
+              <p className="form_title1">Enter your details below.</p>
+            </div>
+            <form onSubmit={loginValidator}>
+              <input
+                autoComplete="off"
+                type="email"
+                name="email"
+                placeholder="E-mail"
+                onChange={handleChangeEmail}
+                required
+              />
+              <input
+                autoComplete="off"
+                type={!showPassword ? 'password' : 'text'}
+                name="password"
+                placeholder="Password"
+                onChange={handleChangePassword}
+                required
+              />
+              {!showPassword ? (
+                <VisibilityOffIcon
+                  onClick={handleClickShowPassword}
+                  className="seen"
+                />
+              ) : (
+                <VisibilityIcon
+                  onClick={handleClickShowPassword}
+                  className="seen"
+                />
+              )}
+              <button className="submit" type="submit">
+                Log In
+              </button>
+            </form>
+            {errors}
+          </div>
+        </div>
+      </main>
+    </Wrapper>
+  );
+};
 
-    )
-})
-
-export default Login
-
+export default Login;
